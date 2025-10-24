4da180d81230370c5dff1acfe3e2ffbbbde57477
Zodac <devblueb@gmail.com>
2025-10-23 21:51:29 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+import PackageDescription
+
+let package = Package(
+    name: "ReverseDynamic",
+    platforms: [
+        .iOS("14.0")
+    ],
+    products: [
+        .library(name: "ReverseDynamic", type: .dynamic, targets: ["ReverseDynamic"])
+    ],
+    targets: [
+        .target(
+            name: "ReverseDynamic",
+            path: "Sources/ReverseDynamic",
+            resources: [
+                .copy("Assets")
+            ]
+        ),
+        .executableTarget(
+            name: "SampleHost",
+            path: "SampleHost/App",
+            dependencies: ["ReverseDynamic"]
+        )
+    ]
+)
